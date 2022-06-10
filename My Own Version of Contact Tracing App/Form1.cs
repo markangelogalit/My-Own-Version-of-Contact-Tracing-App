dbe261a60d44ca89ad0da026b9930f2f9d5412fc
Mark Angelo Galit <rankm12345@gmail.com>
2022-06-10 22:55:01 +0800

    my initial commit

---
+++ My Own Version of Contact Tracing App/Form1.cs
@@ -0,0 +1,10 @@
+namespace My_Own_Version_of_Contact_Tracing_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
