c8bd00c358c725b1fe4c1fab1f5a0a9038cfc6b1
Mark Angelo Galit <rankm12345@gmail.com>
2022-06-10 23:05:59 +0800

    adding labels

--- My Own Version of Contact Tracing App/Form1.cs
+++ My Own Version of Contact Tracing App/Form1.cs
@@ -6,5 +6,20 @@ namespace My_Own_Version_of_Contact_Tracing_App
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CTA1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
