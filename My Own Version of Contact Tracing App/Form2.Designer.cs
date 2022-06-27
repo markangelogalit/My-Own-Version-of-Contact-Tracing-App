7c1941d61d4cc1414abad2fb2ac8f1648480144c
Mark Angelo Galit <rankm12345@gmail.com>
2022-06-27 21:01:05 +0800

    add button, label and textbox and renaming them one by one

--- My Own Version of Contact Tracing App/Form2.Designer.cs
+++ My Own Version of Contact Tracing App/Form2.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.tbx1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(107, 95);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Search";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(58, 53);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(34, 15);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Date ";
+            // 
+            // tbx1
+            // 
+            this.tbx1.Location = new System.Drawing.Point(98, 50);
+            this.tbx1.Name = "tbx1";
+            this.tbx1.Size = new System.Drawing.Size(100, 23);
+            this.tbx1.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(295, 165);
+            this.Controls.Add(this.tbx1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btn1;
+        private Label lbl1;
+        private TextBox tbx1;
     }
 }
